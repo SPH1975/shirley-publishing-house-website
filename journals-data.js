26fd417d9011d24bd4756407c5f328ff70f72d2b
SPH1975 <jossanthvicente1507@gmail.com>
2026-08-10 16:17:22 +0800

    Publish National Research Journal Volume 4 Issue No. 2
    Add 10 Volume 4 Issue No. 2 articles and move Jenny Castro’s article from Issue No. 1.

--- journals-data.js
+++ journals-data.js
@@ -114,7 +114,7 @@ window.SHIRLEY_JOURNALS = [
     ],
     "publicationFrequency": "Semi-annual",
     "format": "Print",
-    "currentVolumeIssue": "",
+    "currentVolumeIssue": "Volume 4 Issue No. 2",
     "establishedYear": "2022",
     "editorInChief": "",
     "editorialBoard": [],
